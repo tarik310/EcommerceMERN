ed1bb733a9146b3f04238daeecfdd79edd7ed108
tarik310 <tareq310harh@gmail.com>
2023-06-19 13:33:00 +0300

    13. Create Loading and Message Component

--- amazona/README.md
+++ amazona/README.md
@@ -49,3 +49,8 @@
 12. Create Product Details Screen
     fetch product from backend
     create 3 columns for image, info and action
+13. Create Loading and Message Component
+    create loading component
+    use spinner component
+    craete message component
+    create utils.js to define getError fuction
--- amazona/frontend/src/screens/ProductScreen.js
+++ amazona/frontend/src/screens/ProductScreen.js
@@ -9,6 +9,9 @@ import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
 const reducer = (state, action) => {
   switch (action.type) {
     case "FETCH_REQUEST":
@@ -37,7 +40,7 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({ type: "FETCH_FAIL", payload: err.message });
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
       //setProducts(result.data);
     };
@@ -45,9 +48,9 @@ function ProductScreen() {
   }, [slug]);
 
   return loading ? (
-    <div>LOADING........</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <div>
       <Row>
